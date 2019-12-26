4bbfb285921766cfabef1c1faa64c3bf314dad57
raymerorale <45501539+raymerorale@users.noreply.github.com>
2019-12-26 18:36:03 +0800

    switch to mlab db

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp-camp",
+mongoose.connect("mongodb+srv://raymer:aakoyun17@cluster0-llkhw.gcp.mongodb.net/test?retryWrites=true&w=majority",
     {
         useNewUrlParser:true, 
         useUnifiedTopology:true,
