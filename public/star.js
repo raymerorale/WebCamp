aca04a9520e64e24d2ee86d70e9ecb4d2b5dd439
raymerorale <45501539+raymerorale@users.noreply.github.com>
2020-01-28 18:18:34 +0800

    mobile responsive

--- public/star.js
+++ public/star.js
@@ -110,3 +110,32 @@ $(LowestPrice).click(function(){
 $(HighestPrice).click(function(){
     $(sortby).val('Highest Price');
 })
+// mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   currentYOffset = self.pageYOffset;
+//   initYOffset = currentYOffset;
+
+//   var intervalId = setInterval(function(){
+//   currentYOffset -= initYOffset*0.05; 
+//   document.body.scrollTop = currentYOffset ;
+//   document.documentElement.scrollTop = currentYOffset;
+
+//     if(self.pageYOffset == 0){
+//       clearInterval(intervalId);
+//     }
+//   }, 20);
+
+// } 
